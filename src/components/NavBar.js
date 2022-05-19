b1324640b9523d7d5805bdaac1a95f5f4aa42e90
Thomas-DCI <thomas.goerldt-freelancer@digitalcareerinstitute.org>
2022-05-19 11:10:49 +0200

    styling WIP

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export const NavBar = () => {
-  return <footer>NavBar</footer>;
+  return (
+    <footer>
+      <div className="footercontainer">
+        <Link to="/highscores">Highscores</Link>
+        <Link to="/imprint">Impressum</Link>
+      </div>
+    </footer>
+  );
 };
