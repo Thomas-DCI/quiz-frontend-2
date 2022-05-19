83de37ce742eeb3d0d037b03c23a656319c8430c
Thomas-DCI <thomas.goerldt-freelancer@digitalcareerinstitute.org>
2022-05-19 13:42:00 +0200

    wip styling

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const Welcome = () => {
-  return <div>Willkommen beim Coder-Quizz!</div>;
+  return (
+    <>
+      <h1>Willkommen beim CoderQuizz!</h1>
+      <p>
+        Beim Coder-Quizz kannst Du Dein Wissen in den Bereichen React und
+        Backend überprüfen.
+      </p>
+      <p>
+        Richtige Antworten geben Punkte, die Du dann am Ende in die
+        Highscore-Liste eintragen kannst.
+      </p>
+      <p>Viel Spaß und hol' Dir den Highscore!</p>
+    </>
+  );
 };
