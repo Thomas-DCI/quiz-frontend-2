5c1fccb945e1574c444b1d1bc94911957e949339
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-05-23 14:55:36 +0200

    Highscore works

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Highscores.css";
 
 export const Highscores = () => {
-  return <div>Highscores</div>;
+  // ----- State for Highscores -----
+  const [highscores, setHighscores] = useState([]);
+
+  // ----- Load Highscores, when loading Component -----
+  useEffect(() => {
+    const loadHighscore = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_HOST}/highscores`
+        );
+        const sortedScores = [...response.data];
+        // Sorts highscore first by points and in case of
+        // same points sorts it then alphabetically
+        sortedScores.sort((a, b) => {
+          if (a.points > b.points) return -1;
+          else if (a.points < b.points) return 1;
+          // if points are the same, sort alphabetically
+          else {
+            if (a.player > b.player) return 1;
+            else return -1;
+          }
+        });
+        setHighscores(sortedScores);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadHighscore();
+  }, []);
+
+  // ----- Render Component -----
+  return (
+    <main className="highscores__container">
+      <h2>Highscores</h2>
+      {/* <div>{JSON.stringify(highscores)}</div> */}
+      <table className="highscores__table">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {highscores.map((highscore, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{highscore.player}</td>
+              <td>{highscore.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
 };
